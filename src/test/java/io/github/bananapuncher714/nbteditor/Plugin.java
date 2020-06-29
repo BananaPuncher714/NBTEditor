00296b65c392a1926779bb4793dbad3b33800102
Hasan Demirtaş <utsukushihito@outlook.com>
2020-06-30 00:16:48 +0300

    plugin test classes

--- src/test/java/io/github/bananapuncher714/nbteditor/Plugin.java
+++ src/test/java/io/github/bananapuncher714/nbteditor/Plugin.java
@@ -1,7 +1,18 @@
 package io.github.bananapuncher714.nbteditor;
 
+import java.io.File;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPluginLoader;
+import org.jetbrains.annotations.NotNull;
 
 public final class Plugin extends JavaPlugin {
+
+    public Plugin() {
+    }
+
+    public Plugin(final @NotNull JavaPluginLoader loader, final @NotNull PluginDescriptionFile description, final @NotNull File dataFolder, final @NotNull File file) {
+        super(loader, description, dataFolder, file);
+    }
 
 }
---
+++ src/test/java/io/github/bananapuncher714/nbteditor/PluginTest.java
@@ -0,0 +1,21 @@
+package io.github.bananapuncher714.nbteditor;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+final class PluginTest {
+
+    @BeforeAll
+    static void initiate() {
+        MockBukkit.mock();
+        MockBukkit.load(Plugin.class);
+
+    }
+
+    @AfterAll
+    static void finish() {
+        MockBukkit.unmock();
+    }
+
+}
